0a122f2e10ae1f13f0f0386bb9d025140a316150
Valeria Hurava <ler4ik65@gmail.com>
2021-11-29 18:03:49 +0100

    working tests

--- src/test/java/ru/netology/Post.java
+++ src/test/java/ru/netology/Post.java
@@ -19,7 +19,7 @@ public class Post {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
